2561411f3ae683bb4543b7d4cebcda511de203ba
pforestivm <pforesti@student.42nice.fr>
2021-11-24 07:16:38 -0500

    final

--- monitoring.sh
+++ monitoring.sh
@@ -18,16 +18,27 @@ pcpu=$(lscpu | grep "CPU(s)" | awk 'NR==1{print $NF}')
 vcpu=$(grep "processor" /proc/cpuinfo | wc -l)
 ram=$(echo "total ${totalram}M used $((usedram*100/totalram))%")
 disk=$(echo "total ${totaldisk}M used $((useddisk*100/totaldisk))%")
+cpu=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
 lastreboot=$(who -b | awk '{print $3 " " $4}')
 lvmactive=$(if [ $lvm -eq 0 ]; then echo "${RED}Not present$NC"; else echo "${GREEN}Present$NC"; fi)
+tcp=$(netstat -s | grep 'active connection openings' | awk '{print $1 " " $2 " " $3}')
+users=$(echo -n "$(users | wc -w) " && echo -n  "connected")
+ip=$(hostname -I)
+mac=$(ip link show | grep "link/ether" | awk '{print $2}')
+cmd=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
 
 #SCRIPT
-echo -e	"${YELLOW}monitoring.sh$NC
-$GRAY#Arc$NC		: $arc
-$GRAY#pCPU$NC		: $pcpu
-$GRAY#vCPU$NC		: $vcpu
-$GRAY#RAM$NC		: $ram
-$GRAY#Disk$NC		: $disk
-$GRAY#Last boot$NC	: $lastreboot
-$GRAY#LVM$NC		: $lvmactive
-"
+wall "$(echo -e "${YELLOW}monitoring.sh$NC")
+$(echo -e "$GRAY#Arc$NC		: $arc")
+$(echo -e "$GRAY#pCPU$NC		: $pcpu")
+$(echo -e "$GRAY#vCPU$NC		: $vcpu")
+$(echo -e "$GRAY#RAM$NC		: $ram")
+$(echo -e "$GRAY#Disk$NC		: $disk")
+$(echo -e "$GRAY#Cpu$NC		: $cpu")
+$(echo -e "$GRAY#Last boot$NC	: $lastreboot")
+$(echo -e "$GRAY#LVM$NC		: $lvmactive")
+$(echo -e "$GRAY#TCP$NC		: $tcp")
+$(echo -e "$GRAY#Users$NC		: $users")
+$(echo -e "$GRAY#IP$NC		: $ip")
+$(echo -e "$GRAY#Mac$NC		: $mac")
+$(echo -e "$GRAY#Sudo cmds$NC	: $cmd")"
